8d818c1ed4fe5c2fa47611720248275c2ee7f35d
Song <217.Songs.SH@gmail.com>
2020-12-10 19:04:03 +0900

    채팅 예제

--- loginSystemNodeJSFinal/src/server.js
+++ loginSystemNodeJSFinal/src/server.js
@@ -3,16 +3,63 @@ var app = express();                // app은 express의 인스턴스
 var cors = require('cors');
 const port = 3001;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.set('view engine', 'ejs')
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Express!</h1>')
-})
 
 var indexRouter = require('./routes/routtest.js');  // 루트 요청
 
 app.use('/login', indexRouter);
+
+
+//Listen on port 3001
+var server = app.listen(port)
+
+app.get("/", (req, res) => {
+    res.send("server is runnning");
+  });
+  
+
+//socket.io instantiation
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	//default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+      // 유저가 생성한 이벤트에 대한 처리 `on`
+    socket.on("sendMessage", (message, callback) => {
+    io.emit("message", { user: user.name, text: message });
+        console.log(message);
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
--- we-chat/src/pages/WSChatPage/WSChatPage.jsx
+++ we-chat/src/pages/WSChatPage/WSChatPage.jsx
@@ -6,8 +6,11 @@ import {
     HeaderContainer,
     SideMenuContainer,
   } from './../../containers';
-import axios from 'axios';
-  
+import io from 'socket.io-client';
+
+let socket;
+const ENDPOINT = "http://localhost:3001";
+
 const WSChatPageContainer = styled.div`
     display: flex;
     width: 100%;
@@ -27,14 +30,21 @@ const WSChatPages = styled.div`
 
 class WSChatPage extends React.Component{
     AAA(){
-        axios.get('http://localhost:3001/')
-        .then((response)=>{
-            console.log(response.data);
-        })
-        .catch((error)=>{
-            console.log(error);
-        });
+        socket = io(ENDPOINT);
+        const str = "hwi";         //버튼을 클릭하면
+    
+        socket.emit("sendMessage", "message is mmm", "o");
 
+
+        (() => {
+            socket.emit('init', { name: 'bella' });
+          
+            socket.on('welcome', (msg) => {
+              console.log(msg);
+            });
+            
+        })();
+        
     }
     
     render(){
